32abf222e15da4843803f5d527a3971fe8f8ded7
angelicalunagar <angelica.lunagar@gmail.com>
2023-11-16 13:15:02 -0600

    Agregué el proyecto con links funcionando

---
+++ src/componentes/Bloque1.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../estilos/Bloque1.css'
+
+function Bloque1(){
+  return(
+    <div className="bloque1-contenedor">
+      <h1>Bloque 1</h1>
+      <h2>Funciones</h2>
+      <h2>Funciones</h2>
+      <h2>Funciones</h2>
+      <h2>Funciones</h2>
+    </div>
+  );
+}
+export default Bloque1;
